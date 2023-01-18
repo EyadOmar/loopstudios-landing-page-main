986b0d820028cc293eff25623ca2f5c3630ba2e3
Eyad Omar <eiad.omar88@gmail.com>
2023-01-18 20:24:14 +0200

    added nav mobile functionality

---
+++ app/JS/nav.js
@@ -0,0 +1,14 @@
+const mainNav = document.querySelector('.mainNav');
+const mobileNavBtn = document.querySelector('.mobile-nav-toggle');
+
+mobileNavBtn.addEventListener('click', () => {
+  console.log('clciked');
+  const visiblity = mainNav.getAttribute('data-visible');
+  if (visiblity === 'false') {
+    mainNav.setAttribute('data-visible', 'true');
+    mobileNavBtn.setAttribute('aria-expanded', 'true');
+  } else {
+    mainNav.setAttribute('data-visible', 'false');
+    mobileNavBtn.setAttribute('aria-expanded', 'false');
+  }
+});
